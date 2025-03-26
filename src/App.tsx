be734c449edf794837fa2a62466d8eaaa6747683
Ali <143140806+its3li@users.noreply.github.com>
2025-03-26 14:31:13 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -70,7 +70,7 @@ function App() {
           'X-Title': 'Fatwa AI',
         },
         body: JSON.stringify({
-          model: 'google/gemini-2.0-pro-exp-02-05:free',
+          model: 'google/gemini-2.5-pro-exp-03-25:free',
           messages: [
             {
               role: 'system',
